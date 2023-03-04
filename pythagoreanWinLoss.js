7d36b6c4be0fad2c9e8caa5b5d8137e93a601921
Jonah Cobbs <jonahcobbs@gmail.com>
2023-03-04 15:59:30 -0700

    implement pythagorean record endpoint

---
+++ pythagoreanWinLoss.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/calculate-pythagorean-wins', (req, res) => {
+  const { gamesPlayed, pointsFor, pointsAgainst } = req.body.Season;
+
+  const pythagoreanWins = Math.round(Math.pow(pointsFor, 2.37) / (Math.pow(pointsFor, 2.37) + Math.pow(pointsAgainst, 2.37)) * gamesPlayed);
+  const pythagoreanLosses = gamesPlayed - pythagoreanWins;
+
+  res.send(`Expected win-loss record: ${pythagoreanWins} - ${pythagoreanLosses}`);
+});
+
+app.listen(port, () => {
+  console.log(`API running on port ${port}`);
+});
